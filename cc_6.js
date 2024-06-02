719666443fb4072d18f07e0a87fb962524d6a3d9
robin-england <robin.england94@googlemail.com>
2024-06-02 13:01:09 -0400

    Implement draw method for Ball class.

--- cc_6.js
+++ cc_6.js
@@ -1,4 +1,14 @@
- class Ball{
+//U63166005
+// Coding Challenge #6: Bouncing Ball Animation Challenge
+
+// 2. Ball Constructor:
+let canvas = document.querySelector("#canvas")
+let ctx = canvas.getContext("2d")
+
+let width = canvas.width
+let height = canvas.height
+
+class Ball{ // Creates ball class
     constructor(x, y, radius, dx, dy, color){
         this.x = x
         this.y = y
@@ -6,5 +16,12 @@
         this.dx = dx
         this.dy = dy
         this.color = color
+    } // Initializes each value
+
+    drawCircle(){  // function to draw a circle
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
+        ctx.fill()
     }
  }
